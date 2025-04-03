29d3eb8b062c0feab8fb6b74713671387ca21599
sakpal-A <akanksha.sakpal@deptagency.com>
2025-04-03 17:11:03 +0530

    update swiper

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -1,1 +1,15 @@
 import Swiper from '../hero-banner/swiper-bundle.min.js';
+
+export default function decorate(block) {
+    block.classList.add('swiper');
+    const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+    Array.from(block.children).forEach(function (row) {
+        row.classList.add('swiper-slide');
+        swiperWrapper.appendChild(row);
+    });
+    block.appendChild(swiperWrapper);
+    Swiper(block, {
+        autoplay: true
+    });
+}
