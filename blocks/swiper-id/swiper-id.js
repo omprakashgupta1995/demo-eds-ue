e538fee183381085a01057b9223b458f56410879
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-03 18:32:54 +0530

    add swiper

---
+++ blocks/swiper-id/swiper-id.js
@@ -0,0 +1,23 @@
+import Swiper from './swiper-bundle.min.js';
+export default function decorate(block) {
+    block.classList.add('swiper');
+    const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+    Array.from(block.children).forEach(function (row) {
+        row.classList.add('swiper-slide');
+        swiperWrapper.appendChild(row);
+    });
+    block.appendChild(swiperWrapper);
+    Swiper(block, {
+        autoplay: true
+    });
+}
+
+
+
+
+
+
+
+
+
