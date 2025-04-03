2187d03d3e8ac4c384a27d266427936b5ffe22c8
Harsh-Patil-Dept <harsh.patil@deptagency.com>
2025-04-03 17:33:56 +0530

    added Hero Banner update

---
+++ blocks/hero-banner/hero-banner.js
@@ -0,0 +1,14 @@
+import Swiper1 from './swiper-bundle.min.js';
+export default function decorate(block) {
+    block.classList.add('swiper');
+    const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+    Array.from(block.children).forEach(function (row) {
+        row.classList.add('swiper-slide');
+        swiperWrapper.appendChild(row);
+    });
+    block.appendChild(swiperWrapper);
+    Swiper1(block, {
+        autoplay: true
+    });
+}
