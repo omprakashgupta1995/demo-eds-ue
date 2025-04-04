ad669e2236dc8bf751685c7fe79cfe32e98977b9
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-04 14:40:36 +0530

    add js file

--- blocks/first-component/first-component.js
+++ blocks/first-component/first-component.js
@@ -1,8 +1,10 @@
 export default async function decorate(block) {
-    
-
     block.forEach((element) => {
-        console.log(element)
+        console.log(element);
+
+        const children = element.children;  
+        Array.from(children).forEach((child) => {
+            console.log(child);  // Log each child
+        });
     });
-    
 }
