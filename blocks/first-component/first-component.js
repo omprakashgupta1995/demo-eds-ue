7e206b0929f65ae104cb3738a6b683519badcf25
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-04 19:32:56 +0530

    add styling

--- blocks/first-component/first-component.js
+++ blocks/first-component/first-component.js
@@ -1,12 +1,24 @@
 export default async function decorate(block) {
-    const children = block.children;  // Get the child elements of the block
 
+    let section = document.querySelector('.first-component-container');
+    console.log('section',section);
+
+    let thirdDivChild = section.children[2];
+    let thirdDivFirstChild = thirdDivChild.children[0];
+    console.log('Abhay',thirdDivFirstChild);
+    thirdDivFirstChild.classList.add('background');
+    // console.log(thirdDivChild);
+    
+     
+    const children = block.children; 
+    
+  
     Array.from(children).forEach((child, index) => {
-       
-        
-      
         child.classList.add(`child-${index + 1}`); 
         console.log(child);  
-    });
+    }); 
+
+
+    
 }
 
