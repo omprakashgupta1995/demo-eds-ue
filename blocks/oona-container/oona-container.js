59bb43c8bb52fcc5f7c5572eb836894aae8086ef
harshnachane05 <harsh.nachane@deptagency.com>
2025-04-07 15:48:48 +0530

    oona-container-updated

--- blocks/oona-container/oona-container.js
+++ blocks/oona-container/oona-container.js
@@ -1,10 +1,12 @@
 export default function decorate(block) {
-    if (block) {
-        // Select all direct child divs inside the block
+    
         const childDivs = block.children;
-        // Loop through and add class with index number
         Array.from(childDivs).forEach((div, index) => {
             div.classList.add(`oona-container${index}`);
         });
-    }
+
+
+        let oonaContainer = document.querySelector('.oona-container0');
+        console.log(oonaContainer);
+        
 }
