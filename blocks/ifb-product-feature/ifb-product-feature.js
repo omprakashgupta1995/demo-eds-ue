e96a42174d24234b6b0f66c79e6c327f8fe8c6a2
Akash-Kushwaha-02 <akash.kushwaha@deptagency.com>
2025-04-07 17:18:30 +0530

    IFB Updated Blocks

--- blocks/ifb-product-feature/ifb-product-feature.js
+++ blocks/ifb-product-feature/ifb-product-feature.js
@@ -5,5 +5,11 @@ export default function decorate(block) {
         divElement.classList.add(`div-container-${index+1}`);
     })  
     console.log(childrenClassName);
+
+    let ifbProductFeatureSection = document.getElementsByClassName('ifb-product-feature-container');
+    console.log(ifbProductFeatureSection);
+    let ifbProductFeatureSectionClassName = Array.from(ifbProductFeatureSection).forEach((divElement, index) =>{
+        divElement.classList.add(`ifb-product-feature-container-${index+1}`)
+    })        
     
 }
