f702b35ad898d1465ef323fa84a8c7673f2bb9ad
Harsh-Patil-Dept <harsh.patil@deptagency.com>
2025-04-08 18:51:09 +0530

    updated FnQ block

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,5 +1,5 @@
 import buildAccordianBlock from '../accordion/accordion.js';
-import {ul,ol,li,div} from '../../scripts/domhelper.js'
+import {ul,li,div} from '../../scripts/domhelper.js'
 export default async function decorate(block) {
     console.log(block);
     const path = block.textContent.trim();
