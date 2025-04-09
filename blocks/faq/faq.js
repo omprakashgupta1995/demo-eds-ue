f50808646d6a7a32704c2a2b4453e5c8cf32d0af
Om Gupta <om.gupta@teknopoint.in>
2025-04-09 16:52:12 +0530

    section id added

---
+++ blocks/faq/faq.js
@@ -0,0 +1,29 @@
+import { div, li, ul } from "../../scripts/dom-helpers.js";
+import buildAccordianBlock from '../accordion/accordion.js';
+
+export default async function decorate(block) {
+    console.log(block);
+    const a = block.querySelector('a');
+    const path = a?.href.trim();
+    // a.remove();
+    const resp = await fetch(path);
+    const respData = await resp.json();
+    const data = respData.data;
+    const list = [li('All')];
+    const accordianBlock = div({class : 'block accordion'},
+        ...data.map(function (eachdata) {
+            list.push(li(eachdata.category));
+            return div( 
+                div(eachdata.title),
+                div(eachdata.description)
+            )
+        })
+    )
+    const title = block.children[1];
+    block.textContent = '';
+    block.append(ul(...list));
+    block.append(title);
+    // block.append(ul(...list));
+    buildAccordianBlock(accordianBlock);
+    block.append(accordianBlock);
+}
