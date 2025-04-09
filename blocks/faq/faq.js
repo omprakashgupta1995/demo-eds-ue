4bec8cc4e70fa6846519970511e3e6f3d875d7d6
Akash-Kushwaha-02 <akash.kushwaha@deptagency.com>
2025-04-09 11:47:31 +0530

    Added Accordion

---
+++ blocks/faq/faq.js
@@ -0,0 +1,24 @@
+import buildAccordianBlock from '../accordion/accordion';
+import {ul,li,div} from '../../scripts/domhelper'
+
+export default async function decorate(block) {
+    console.log(block);
+    const path = block.textContent.trim();
+    const resp = await fetch(path);
+    const respData = await resp.json();
+    const data = respData.data;
+    block.textContent = '';
+    const list = [li('All')];
+    const accordianBlock = div({class : 'block accordion'},
+      ...data.map(function (eachdata) {
+        list.push(li(eachdata.category));
+        return div(
+          div(eachdata.title),
+          div(eachdata.description)
+        )
+      })
+    )
+    block.append(ul(...list));
+    buildAccordianBlock(accordianBlock);
+    block.append(accordianBlock);
+  } 
--- blocks/ifb-product-feature/ifb-product-feature.js
+++ blocks/ifb-product-feature/ifb-product-feature.js
@@ -10,6 +10,19 @@ export default function decorate(block) {
     console.log(ifbProductFeatureSection);
     let ifbProductFeatureSectionClassName = Array.from(ifbProductFeatureSection).forEach((divElement, index) =>{
         divElement.classList.add(`ifb-product-feature-container-${index+1}`)
-    })        
+    }) 
     
+    
+    //IFB-Hero-Banner
+    let ifbHeroBannerContainer = document.querySelector('.ifb-hero-banner div').children
+    console.log('HERO-Container',ifbHeroBannerContainer);
+    
+    ifbHeroBannerContainer[0].classList.add('ifb-hero-image')
+    ifbHeroBannerContainer[1].classList.add('ifb-hero-heading-text')
+    ifbHeroBannerContainer[2].classList.add('ifb-hero-btn-text-info')
+    ifbHeroBannerContainer[3].classList.add('ifb-hero-btn')
+
+
+
+
 }
