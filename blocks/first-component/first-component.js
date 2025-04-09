3bbb9fa4c3ba00828806f7332e1c5c64dac99f60
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-09 11:01:19 +0530

    add faq

--- blocks/first-component/first-component.js
+++ blocks/first-component/first-component.js
@@ -1,14 +1,26 @@
 export default async function decorate(block) {
 
+    console.log(block.children);
+
+    let blockFirstDiv = block.children[0];
+    blockFirstDiv.classList.add('flex-block-container');
+
+    Array.from(blockFirstDiv.children).forEach((divElement, index)=>
+    {
+        divElement.classList.add(`flex-block-container-${index+1}`)
+    })
+
     let section = document.querySelector('.first-component-container');
     console.log('section',section);
 
-    let thirdDivChild = section.children[2];
-    let thirdDivFirstChild = thirdDivChild.children[0];
+    let thirdDivChild = section.children;
+    // console.log("dec",thirdDivChild)
+    let thirdDivFirstChild = thirdDivChild[1];
     console.log('Abhay',thirdDivFirstChild);
-    thirdDivFirstChild.classList.add('background');
-    // console.log(thirdDivChild);
-    
+    let backgroundChild = thirdDivFirstChild.children[0]
+    let backgroundColor = backgroundChild.children[0]
+    backgroundColor.classList.add('background');
+
      
     const children = block.children; 
     
