71094b8a03cc7c007a10ae651fbe456673ef3d91
sakpal-A <akanksha.sakpal@deptagency.com>
2025-04-09 16:39:10 +0530

    add js to faq js file

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,7 +1,7 @@
 import buildAccordianBlock from '../accordion/accordion.js';
-import {li, ul, div} from '../../scripts/domhelper.js'; 
+import { li, ul, div } from '../../scripts/domhelper.js';
 
-export default async function decorate(block) {
+/* export default async function decorate(block) {
     console.log(block);
     const path = block.textContent.trim();
     const resp = await fetch(path);
@@ -21,4 +21,27 @@ export default async function decorate(block) {
     block.append(ul(...list));
     buildAccordianBlock(accordianBlock);
     block.append(accordianBlock);
-  }
+  } */
+
+export default async function decorate(block) {
+  const path = block.textContent.trim();
+  const resp = await fetch(path);
+  const respData = await resp.json();
+  const data = respData.data;
+  const list = [li('All')];
+  const accordianBlock = div({ class: 'block accordion' },
+    data.map(function (eachdata) {
+      list.push(li(eachdata.category));
+      return div(
+        div(eachdata.title),
+        div(eachdata.description)
+      )
+    })
+  )
+  const title = block.children[1];
+  block.textContent = '';
+  block.append(ul(...list));
+  block.append(title);
+  buildAccordianBlock(accordianBlock);
+  block.append(accordianBlock);
+}
