f81e0900b8c599e24e6b91b6231656534405bd1e
Harsh-Patil-Dept <harsh.patil@deptagency.com>
2025-04-16 11:58:20 +0530

    Added kansai banner

--- blocks/kainsai-teaser/kainsai-teaser.js
+++ blocks/kainsai-teaser/kainsai-teaser.js
@@ -1,4 +1,28 @@
 /* eslint-disable */
 export default function decorate(block) {
-  console.log('kainsai-teaser loadded', block);
+  const divs = Array.from(block.children);
+  
+
+  if (divs.length > 0) {
+    divs[0].classList.add('mobile-Image');
+    divs[1].classList.add('desktop-Image');
+    divs[2].classList.add('teaser-content');
+  }
+
+  if (divs.length > 1 && divs[1].children.length > 0) {
+    const parasContainer = Array.from(divs[1].children);
+    const paras = parasContainer.flatMap((container) => Array.from(container.children));
+    // console.log(paras);
+    paras[0].classList.add('title');
+    paras[1].classList.add('description');
+    paras[2].classList.add('sub-title');
+    paras[3].classList.add('btn-Container');
+    paras[4].classList.add('color-n-sku');
+
+    const colorsku = Array.from(paras[4].children);
+    colorsku[0].classList.add('color-container');
+    colorsku[1].classList.add('sku-container');
+
+    paras[5].classList.add('get-btn-Container');
+  }
 }
