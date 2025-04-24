356c430179186b3a42fbb8a81fc597f80514d5bd
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-24 11:50:30 +0530

    add banner swiper

--- blocks/favourite-corner/favourite-corner.js
+++ blocks/favourite-corner/favourite-corner.js
@@ -1,12 +1,14 @@
 export default async function decorate(block) {
   const favouriteChild = Array.from(block.children);
-  favouriteChild.forEach((element, index) => {
-    element.classList.add(`child-${index}`); // Adds class like child-0, child-1, etc.
+
+  favouriteChild.forEach((element) => {
+    element.classList.add('favourite-corner-block');
+
+    // Get children of each favourite-corner-block
+    const innerChildren = Array.from(element.children);
+    innerChildren.forEach((child, childIndex) => {
+      child.classList.add(`favourite-inner-${childIndex}`);
+      // Example: favourite-inner-0-0, favourite-inner-0-1, etc.
+    });
   });
-  const picTag = Array.from(favouriteChild[0].children);
-
-picTag.forEach((el) => {
-  el.classList.add("pic");
-});
-
 }
---
+++ blocks/hero-banner-slider/hero-banner-slider.js
@@ -0,0 +1,59 @@
+/*eslint-disable */
+import loadEmbed from '../embed/embed.js';
+import Swiper from './swiper-bundle.min.js';
+export default function decorate(block) {
+
+  block.classList.add('swiper');
+
+
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.classList.add('swiper-wrapper');
+
+  Array.from(block.children).forEach((row) => {
+    const typeEl = row.firstElementChild;
+    const type = typeEl.textContent.trim().toLowerCase(); 
+    typeEl.remove();
+    if(type === 'embed'){
+      loadEmbed(row);
+    }
+    row.classList.add('swiper-slide');
+    swiperWrapper.appendChild(row);
+  });
+
+  block.appendChild(swiperWrapper);
+  Swiper(block, {
+    autoplay: true,
+  },);
+
+  const childDivs = Array.from(block.children);
+
+  // Step 1: Add banner-1, banner-2, etc.
+  childDivs.forEach((div, index) => {
+    div.classList.add(`banner-${index + 1}`);
+  });
+
+  // Step 2: Remove empty inner divs from each .banner-* block
+  childDivs.forEach((bannerDiv) => {
+    const innerDivs = bannerDiv.querySelectorAll("div");
+    innerDivs.forEach((innerDiv) => {
+      if (innerDiv.innerHTML.trim() === '') {
+        // innerDiv.remove();
+      }
+    });
+  });
+  const navContainer = document.createElement('div');
+  navContainer.classList.add('slider-nav');
+
+  const prevButton = document.createElement('button');
+  prevButton.classList.add('slider-btn', 'prev-btn');
+  prevButton.innerHTML = '<';
+
+  const nextButton = document.createElement('button');
+  nextButton.classList.add('slider-btn', 'next-btn');
+  nextButton.innerHTML = '>';
+
+  navContainer.appendChild(prevButton);
+  navContainer.appendChild(nextButton);
+  block.appendChild(navContainer);
+
+}
