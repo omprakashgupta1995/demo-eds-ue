92235b56c44bf0b8909358239e49968a166d80d7
Yash-Kudve <yash.kudve@deptagency.com>
2025-04-24 22:55:38 +0530

    spotlight item 1 added

---
+++ blocks/idfc-spotlight/idfc-spotlight.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function decorate(block){
+    const childDivs = Array.from(block.children);
+
+  childDivs.forEach((div, index) => {
+    div.classList.add(`spotlight-div`)
+    div.classList.add(`spotlight-item-${index + 1}`);
+  });
+}
