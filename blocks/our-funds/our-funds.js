cfdc1ea6828a7b28803333691fa0ea009bd4fad5
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-29 17:43:24 +0530

    add another item

---
+++ blocks/our-funds/our-funds.js
@@ -0,0 +1,10 @@
+export default function decorate(block) {
+  const blockChild = block.children;
+  const list = blockChild[0];
+  const last = list.children;
+
+  // Loop through the children and add class "list"
+  Array.from(last).forEach((child) => {
+    child.classList.add('li-list');
+  });
+}
