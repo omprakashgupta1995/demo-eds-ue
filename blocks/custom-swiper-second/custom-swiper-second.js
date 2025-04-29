7c42ecd7045611ac8c5e6773a26812b31ffb4018
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-29 15:36:31 +0530

    add one more component

--- blocks/custom-swiper-second/custom-swiper-second.js
+++ blocks/custom-swiper-second/custom-swiper-second.js
@@ -1,12 +1,5 @@
-// import loadEmbed from '../video/video.js';
+/* eslint-disable*/
 import swiperFirst from '../custom-swiper/custom-swiper.js';
-
 export default function decorate(block) {
-  swiperFirst(block);
-//     const swiperSecond = block.children;
-//    Array.from(swiperSecond).forEach((ele)=>{
-//     const hideElement = ele.children;
-//   const hide = hideElement[0];
-//   hide.style.display = "none";
-//    })
+    swiperFirst(block);
 }
