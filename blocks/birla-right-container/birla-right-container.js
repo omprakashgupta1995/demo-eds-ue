17662fabe813357100e661889b3d47b159fc6814
Harsh-Patil-Dept <harsh.patil@deptagency.com>
2025-05-11 18:35:26 +0530

    yash restructure

--- blocks/birla-right-container/birla-right-container.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable */
-export default function decorate(block) {
-  const childDivs = block.children;
-
-  // Loop through and add class with index number
-  Array.from(childDivs).forEach((div, index) => {
-    div.classList.add(`div-index-${index}`);
-  });
-}
