7ad5b1e61cb5a9b6ccd108bf0f408051cf2dbe6c
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-05-15 12:35:56 +0530

    add lable

---
+++ blocks/calculator/calculator.js
@@ -0,0 +1,7 @@
+import { div,h1,p,input,label} from '../../scripts/domhelper.js';
+export default async function decorate(block) {
+
+    const mainContainer = block;
+    console.log(mainContainer);
+
+}
