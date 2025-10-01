b879031afb79ebf367ee689707f3a0c8fd6eee1a
Yash-Kudve <yash.kudve@deptagency.com>
2025-10-01 10:16:46 +0530

    changes added

--- scripts/dom-helpers.js
+++ scripts/dom-helpers.js
@@ -97,3 +97,5 @@ export function article(...items) { return domEl('article', ...items); }
 export function strong(...items) { return domEl('strong', ...items); }
 export function select(...items) { return domEl('select', ...items); }
 export function option(...items) { return domEl('option', ...items); }
+export function script(...items) { return domEl('script', ...items); }
+export function linkEl(...items) { return domEl('link', ...items); }
