14ad08626558b96a5c3447c99cad68b00a7b6ca3
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-11-06 10:40:16 +0530

    added one block for idfc bank

---
+++ blocks/calculator-ret-graf/calculator-ret-graf.js
@@ -0,0 +1,20 @@
+import {
+  div,
+  h1,
+  p,
+  input,
+  label,
+  span,
+  button,
+} from "../../scripts/domhelper.js";
+
+export default async function decorate(block) {
+
+
+    const retCal = ({class : 'main-container'},
+
+    );
+
+
+    block.append(retCal);
+}
