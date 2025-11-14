24450826b27acb15a8b446c45a464b1f1a5d8c20
pratikchauhan-developer <pratik.chauhan@deptagency.com>
2025-11-14 11:00:35 +0530

    swiper

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,7 +1,14 @@
 import Swiper from "../../scripts/swiper-bundle.min.js";
+import { div } from "../../scripts/dom-helpers.js";
 
 
 export default function decorate(block) {
    console.log("block", block);
+   const swiperContent = div({ class: "swiper" },
+      ...Array.from(block.children).map(child => {
+         return div({ class: "swiper-slide" }, child)
+      })
+   )
 
+   block.appendChild(swiperContent);
 }
