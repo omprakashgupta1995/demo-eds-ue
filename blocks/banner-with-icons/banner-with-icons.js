4afcd25b8c37a41d4ecac0a020b5ce74372b9cf3
pranav-telangade-dept <pranav.telangade@deptagency.com>
2025-11-14 08:52:08 +0530

    changes'

--- blocks/banner-with-icons/banner-with-icons.js
+++ blocks/banner-with-icons/banner-with-icons.js
@@ -9,4 +9,19 @@ export default function decorate(block) {
     .querySelector(".content_wrapper")
     .children[1].classList.add("right_image");
   block.querySelector(".left_content").children[0].classList.add("heading");
+
+
+//   let pointDiv =  document.createElement("div");
+
+//   pointDiv.classList.add("point")
+
+
+
+  Array.from(block.querySelector(".left_content").children).forEach(
+    (child, i) => {
+      if (i > 0) {
+        child.classList.add("point_content");
+      }
+    }
+  );
 }
