52cdf2c98ae827a30fae9bed5756e744edeb3b89
TEKNOPOINT\Ashish.Patil <ashish.patil@teknopoint.com>
2025-11-14 11:16:55 +0530

    ädded swiper script

---
+++ blocks/my-swiper/my-swiper.js
@@ -0,0 +1,21 @@
+import Swiper from '../../scripts/swiper-bundle.min.js';
+import { div } from '../../scripts/dom-helpers.js';
+
+
+export default function decorate(block) {
+    const swiperE1 = div(
+        { class: 'swiper' },
+        div(
+            { class: 'swiper-wrapper' },
+            ...Array.from(block.children).map((child) => {
+                child.classList.add('swiper-slide');
+                return child;
+            });
+        );
+    );
+    block.appendChild(swiperE1);
+    Swiper(swiperE1, {
+        direction: 'vertical',
+        loop: true
+    });
+}
