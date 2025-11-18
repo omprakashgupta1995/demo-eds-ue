5f3a9de319bed2c509cc09972bb04a2d915326ea
pratikchauhan-developer <pratik.chauhan@deptagency.com>
2025-11-18 21:07:13 +0530

    changes

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,17 +1,19 @@
-import Swiper from "../../scripts/swiper-bundle.min.js";
-import { div } from "../../scripts/dom-helpers.js";
-
+import Swiper from '../../scripts/swiper-bundle.min.js';
+import { div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
-   console.log("block", block);
-   const swiperContent = div({ class: "swiper" },
-      ...Array.from(block.children).map(child => {
-         child.classList.add("swiper-slide")
-         return child;
-      })
-   )
+   const swiperContent = div(
+      { class: 'swiper' },
+      div(
+         { class: 'swiper-wrapper' },
+         ...Array.from(block.children).map((child) => {
+            child.classList.add('swiper-slide');
+            return child;
+         }),
+      ),
+   );
 
    block.appendChild(swiperContent);
 
-   Swiper(swiperContent, {})
+   Swiper(swiperContent);
 }
