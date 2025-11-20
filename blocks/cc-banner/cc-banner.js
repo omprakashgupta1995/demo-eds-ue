7d8952bd1e72602d6ec33f93d0918793e5bb20a9
TEKNOPOINT\Ashish.Patil <ashish.patil@teknopoint.com>
2025-11-20 14:53:48 +0530

    added css

--- blocks/cc-banner/cc-banner.js
+++ blocks/cc-banner/cc-banner.js
@@ -4,6 +4,9 @@ export default function decorate(block) {
     Array.from(block.children).forEach((el,ind) => {
         let item = ind == 0 ? el.classList.add('banner-image') : ind == 1 ? el.classList.add('banner-heading') : ind ==2 ? el.classList.add('banner-desc') : ind == 3 ? el.classList.add('banner-fea') : false;
         if(ind == 3){
+            Array.from(el.children).forEach((e, ind) => {
+                let item = e.classList.add('feature-items');
+            });
             console.log(el.children);
         }
     });
