461f3b81a3f25480ddce31100a7850b74156b7d7
Rakesh Gupta <Rakesh.Gupta@teknopoint.local>
2025-11-21 10:23:55 +0530

    whatsapp banner updated

--- blocks/whatsapp-banner/whatsapp-banner.js
+++ blocks/whatsapp-banner/whatsapp-banner.js
@@ -1,6 +1,4 @@
 export default function decorate() {
     console.log('Hello World');
 
-    let bannerWrapper = document.getElementsByClassName('whatsapp-banner')
-    bannerWrapper.parentElement.children[1].secondChild.classList.add('banner-content')
 }
