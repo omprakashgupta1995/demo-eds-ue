ecfd880aa7f2f1d5fb0d819c5e067ab23b15ca58
Beauty Singh <Beauty.Singh@teknopoint.local>
2025-11-21 09:57:31 +0530

    added swiper change

--- blocks/Swiper/Swiper.js
+++ blocks/Swiper/Swiper.js
@@ -1,17 +1,19 @@
-import swiperMin from "../../scripts/swiper.min.js";
-import { div } from "../../scripts/dom-helpers.js";
+import swiperMin from '../../scripts/swiper.min.js';
+import { div } from '../../scripts/dom-helpers.js';
 
-export default  function decorate(block) {
-    console.log(block);
-    const swiperEl = div({class:"swiper"},
-       div({class:'swipper-wrapper'},
-         ...Array.from(block.children).map(child => {
-            child.classList.add("swiper-slide")
-            return child;
-        })
-       )
-    )
+export default function decorate(block) {
+  console.log(block);
+  const swiperEl = div(
+    { class: 'swiper' },
+    div(
+      { class: 'swiper-wrapper' },
+      ...Array.from(block.children).map((child) => {
+        child.classList.add('swiper-slide');
+        return child;
+      }),
+    ),
+  );
 
-    block.appendChild(swiperEl);
-    swiperMin(swiperEl);
+  block.appendChild(swiperEl);
+  swiperMin(swiperEl);
 }
