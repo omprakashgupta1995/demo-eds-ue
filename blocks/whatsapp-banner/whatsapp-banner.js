48897da3438d264c36ba24a86428e412aace3f09
Rakesh Gupta <Rakesh.Gupta@teknopoint.local>
2025-11-21 10:30:16 +0530

    whatsapp banner updated

--- blocks/whatsapp-banner/whatsapp-banner.js
+++ blocks/whatsapp-banner/whatsapp-banner.js
@@ -1,4 +1,14 @@
-export default function decorate() {
-    console.log('Hello World');
-
+import { div } from "../../scripts/dom-helpers.js";
+export default async function decorate(block) {
+    console.dir(block);
+    Array.from(block.children).forEach((rowElm, rowIndex) => {
+        rowElm.classList.add(`row-${rowIndex + 1}`);
+        Array.from(rowElm.children).forEach((colElm, colIndex) => {
+            colElm.classList.add(`col-${colIndex + 1}`);
+        })
+    })
+    const row4 = block.querySelector(".row-4");
+    const row5 = block.querySelector(".row-5")
+    const parentDiv = div({ class: "parent_row4_row5", }, ...[row4, row5])
+    block.appendChild(parentDiv)
 }
