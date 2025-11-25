418c5aca1bb7cdec909c35b73c526a267b2c1498
sumitrai-debug <sumit.rai@deptagency.com>
2025-11-25 22:30:39 +0530

    test commit added

--- blocks/product-banner/product-banner.js
+++ blocks/product-banner/product-banner.js
@@ -1,19 +1,19 @@
-// export default function decorate(block){
-//     // block.children[1].style.display="none";
-//     console.log(block.children);
-//     const contentOverBanner = [];
-//     const bannerEl = Array.from(block.children).filter((el,ind)=>{
-//         if(ind==1){
-//         } else if (ind>1){
-//             contentOverBanner.push(el);
-//         } else {
-//             return el;
-//         }
-//     });
-//     const flexWrapper = document.createElement('div');
-//     flexWrapper.append(contentOverBanner[0],contentOverBanner[1]);
-//     block.innerHTML = '';
-//     block.append(bannerEl[0]);
-//     block.append(flexWrapper);
+export default function decorate(block){
+    // block.children[1].style.display="none";
+    console.log(block.children);
+    const contentOverBanner = [];
+    const bannerEl = Array.from(block.children).filter((el,ind)=>{
+        if(ind==1){
+        } else if (ind>1){
+            contentOverBanner.push(el);
+        } else {
+            return el;
+        }
+    });
+    const flexWrapper = document.createElement('div');
+    flexWrapper.append(contentOverBanner[0],contentOverBanner[1]);
+    block.innerHTML = '';
+    block.append(bannerEl[0]);
+    block.append(flexWrapper);
 
-// }
+}
