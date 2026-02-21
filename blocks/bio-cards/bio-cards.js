e964be643b20052c6b61688588a78a3d88bafd2f
ChiragParmar2024 <chirag.parmar@deptagency.com>
2026-02-21 15:05:34 +0530

    progress saving

---
+++ blocks/bio-cards/bio-cards.js
@@ -0,0 +1,22 @@
+// blocks/bio-cards/bio-cards.js
+
+export default function decorate(block) {
+  [...block.children].forEach(card => {
+    card.classList.add('bio-cards-slide');
+
+    const [imgWrapper, contentWrapper] = [...card.children];
+    if (!imgWrapper || !contentWrapper) return;
+
+    imgWrapper.classList.add('bio-card-img');
+    contentWrapper.classList.add('bio-card-content');
+
+    const [name, designation, description, buttonContainer] = [
+      ...contentWrapper.children,
+    ];
+
+    name?.classList.add('bio-card-name');
+    designation?.classList.add('bio-card-designation');
+    description?.classList.add('bio-card-description');
+    buttonContainer?.classList.add('bio-card-btn-text');
+  });
+}
