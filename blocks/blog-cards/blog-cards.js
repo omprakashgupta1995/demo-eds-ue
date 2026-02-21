6ed3677a90c098c9cf515141b6a8bdd4c14b0f9d
pooja.pramod <Pooja.Pramod@teknopoint.local>
2026-02-21 14:54:35 +0530

    added blog card

---
+++ blocks/blog-cards/blog-cards.js
@@ -0,0 +1,5 @@
+export default function decorate(block) {
+  console.log('Blog Cards block loaded!');
+  console.log('Block element:', block);
+  console.log('Children (cards):', block.children);
+}
