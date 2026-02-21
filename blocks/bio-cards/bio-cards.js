2fd83cefd2a87a685cc0d68f8bccaac1474c3665
Shubham Raut <shubham.raut@deptagency.com>
2026-02-21 14:56:04 +0530

    feat: add bio-cards

---
+++ blocks/bio-cards/bio-cards.js
@@ -0,0 +1,6 @@
+export default function main(block) {
+    console.log('Bio Cards block initialized', block);
+    console.log('Bio Cards block data:', block.data);
+    console.log('Bio Cards block innerHTML:', block.innerHTML);
+    console.log('Bio Cards block children:', block.children);
+}
