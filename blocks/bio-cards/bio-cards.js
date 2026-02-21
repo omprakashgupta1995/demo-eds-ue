8144ad926c8bc9f03aa344e59f3d11d78860d676
Shravan Yadav <Shravan.Yadav@teknopoint.local>
2026-02-21 15:03:21 +0530

    bio card block added

---
+++ blocks/bio-cards/bio-cards.js
@@ -0,0 +1,6 @@
+
+export default function decorate(block) {
+  console.log('Bio Cards block loaded!');
+  console.log('Block element:', block);
+  console.log('Children (cards):', block.children);
+}
