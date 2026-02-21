75878dfb00616648916bebeef033340d73b8d634
anjali giri <anjali.giri1206@gmail.com>
2026-02-21 15:01:44 +0530

    block created

---
+++ blocks/anjali/anjali.js
@@ -0,0 +1,8 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+ console.log(block);
+ 
+}
