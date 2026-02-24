e11fd87cb7b4c02b0fb4b8b36f497003e3c4229f
Sumeet Singh <Sumeet.Singh@teknopoint.local>
2026-02-24 18:29:39 +0530

    css changes

--- blocks/deposit-cards/deposit-cards.js
+++ blocks/deposit-cards/deposit-cards.js
@@ -1,10 +1,9 @@
-
 const depClass = document.querySelectorAll('.deposit-cards > div');
 
-const depbod = document.querySelectorAll('.dep-card');
 export default function decorate(block) {
   depClass.forEach(child => {
     child.classList.add('dep-card');
+    const depbod = document.querySelectorAll('.dep-card');
     depbod.forEach(child=>{
     const { children } = child;
     if(children[0]) children[0].classList.add('card-img');
