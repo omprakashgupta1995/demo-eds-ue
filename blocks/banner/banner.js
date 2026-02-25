8508619d08ab3efe0ad39c8db3c159730ebbb924
RAJESH GUPTA <rajesh.gupta@teknopoint.in>
2026-02-25 11:47:47 +0530

    swiper class added on banner

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,3 +1,27 @@
 export default function decorate(block) {
-    console.log(block)
+    console.log(block);
+    block.classList.add("banner-section");
+    [...block.children].forEach((slide) => {
+        slide?.classList.add("swiper-slide")
+        let [imgWrapper, contentWrapper] = [...slide.children];
+        imgWrapper?.classList.add("banner-img-wrapper");
+        contentWrapper?.classList.add("banner-cnt-wrapper");
+        let [title, pretitle, cta, description] = [...contentWrapper.children];
+        title?.classList.add("banner-title");
+        pretitle?.classList.add("banner-pretitle");
+        cta?.classList.add("banner-cta-wrapper");
+        description?.classList.add("banner-description")
+    })
+    let blockChildren = [...block.children];
+
+    let swiperWrapper = document.createElement("div");
+    swiperWrapper.classList.add("swiper-wrapper");
+
+    let pagination = document.createElement("div");
+    pagination.classList.add("swiper-pagination");
+
+    swiperWrapper.appendChild(blockChildren)
+    block.innerHTML = "";
+    block.appendChild(swiperWrapper)
+    block.appendChild(pagination)
 }
