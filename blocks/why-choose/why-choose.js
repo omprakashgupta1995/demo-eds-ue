45e13eb82bf50f1f11ce508a98535a75092fc870
ronak patel <ronak.patel@teknopoint.in>
2026-03-04 15:58:21 +0530

    component changes

--- blocks/why-choose/why-choose.js
+++ blocks/why-choose/why-choose.js
@@ -1,4 +1,22 @@
 export default function decorate(block) {
 
+  const link = block.querySelector('.know_more_btn a');
+
+  if (link) {
+
+    // Add class to link
+    link.classList.add('know-more-btn');
+
+    // Prevent duplicate icon if reloaded
+    if (!link.querySelector('img')) {
+
+      const icon = document.createElement('img');
+      icon.src = '/content/dam/ifiwebsite/icons/arrow-right-circle.png';
+      icon.alt = 'arrow';
+      icon.className = 'arrow-icon';
+
+      link.appendChild(icon);
+    }
+  }
 
 }
