3774d80dd2e44858f864dc8985a240f74f5cc630
ronak patel <ronak.patel@teknopoint.in>
2026-03-04 16:06:07 +0530

    component changes

--- blocks/why-choose/why-choose.js
+++ blocks/why-choose/why-choose.js
@@ -1,22 +1,16 @@
 export default function decorate(block) {
 
-  const link = block.querySelector('.know_more_btn a');
+  // const link = block.querySelector('.know_more_btn a');
 
-  if (link) {
-
-    // Add class to link
-    link.classList.add('know-more-btn');
-
-    // Prevent duplicate icon if reloaded
-    if (!link.querySelector('img')) {
-
-      const icon = document.createElement('img');
-      icon.src = '/content/dam/ron-ifl/arrow-right-circle.png';
-      icon.alt = 'arrow';
-      icon.className = 'arrow-icon';
-
-      link.appendChild(icon);
-    }
-  }
+  // if (link) {
+  //   link.classList.add('know-more-btn');
+  //   if (!link.querySelector('img')) {
+  //     const icon = document.createElement('img');
+  //     icon.src = '/content/dam/ron-ifl/arrow-right-circle.png';
+  //     icon.alt = 'arrow';
+  //     icon.className = 'arrow-icon';
+  //     link.appendChild(icon);
+  //   }
+  // }
 
 }
