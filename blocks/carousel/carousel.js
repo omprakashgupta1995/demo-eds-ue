a22f624f76ea972877961c8c365b2f57fdcda75e
ronak patel <ronak.patel@teknopoint.in>
2026-03-23 12:40:48 +0530

    carousel changes done

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,89 @@
+import Swiper from '../../scripts/swiper.min.js';
+export default function decorate(block) {
+  if (!block) return;
+
+  /* ---------------- SWIPER STRUCTURE ---------------- */
+
+  block.classList.add('swiper');
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'swiper-wrapper';
+
+  const slides = Array.from(block.children);
+
+  slides.forEach((slide) => {
+    slide.classList.add('swiper-slide');
+    wrapper.appendChild(slide);
+  });
+
+  block.innerHTML = '';
+  block.appendChild(wrapper);
+
+  /* ---------------- ARROWS ---------------- */
+
+  const navWrapper = document.createElement('div');
+  navWrapper.className = 'cards-nav';
+
+  const prev = document.createElement('div');
+  prev.className = 'swiper-button-prev';
+
+  const next = document.createElement('div');
+  next.className = 'swiper-button-next';
+
+  navWrapper.append(prev, next);
+
+  const container = block.closest('.carousel-container');
+  const titleWrapper = container?.querySelector('.default-content-wrapper');
+
+  if (titleWrapper) {
+    titleWrapper.append(navWrapper);
+  } else {
+    block.parentElement.append(navWrapper);
+  }
+// pagination 
+const paginationEl = document.createElement('div');
+paginationEl.className = 'swiper-pagination';
+
+// append inside slider
+block.appendChild(paginationEl);
+  /* ---------------- SWIPER INIT ---------------- */
+
+  new Swiper(block, {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    loop: false,
+
+    navigation: {
+      nextEl: next,
+      prevEl: prev,
+    },
+    pagination: {
+    el: paginationEl,
+    clickable: true,
+    enabled: true, // default mobile ON
+  },
+     breakpoints: {
+    0: {
+      slidesPerView: 1.75,  
+      spaceBetween: 16,
+      pagination: {
+        enabled: true,   // ✅ mobile ON
+      },
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      pagination: {
+        enabled: false,   
+      },
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      pagination: {
+        enabled: false,   
+      },
+    }
+  }
+  });
+}
