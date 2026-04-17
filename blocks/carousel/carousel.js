e4e0d1443f46e3f3f4ae8c33574d15017538db68
Nandaniraj Singh <Nandaniraj.Singh@teknopoint.local>
2026-04-17 17:52:59 +0530

    accordion block edited

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,3 +1,19 @@
+import swiper from "./swiper.min.js";
+
 export default function decorate(block) {
-  console.log(block.outerHTML);
+  block.parentElement.classList.add("swiper");
+  let blockParent = block.parentElement;
+  block.classList.add("swiper-wrapper");
+  Array.from([...block.children]).forEach((items) => {
+    items.classList.add("swiper-slide");
+  });
+  let swiperPagination = document.createElement("div");
+  swiperPagination.classList.add("swiper-pagination");
+  blockParent.appendChild(swiperPagination);
+
+  swiper(blockParent, {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 }
