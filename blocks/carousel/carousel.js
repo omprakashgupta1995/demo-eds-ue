b724b0329d67e6e1e0575a0c807b918d26671dff
Nandaniraj Singh <Nandaniraj.Singh@teknopoint.local>
2026-04-17 18:21:57 +0530

    carousel js added

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,6 +1,7 @@
 import swiper from "./swiper.min.js";
 
 export default function decorate(block) {
+  console.log(block);
   block.parentElement.classList.add("swiper");
   let blockParent = block.parentElement;
   block.classList.add("swiper-wrapper");
