60336b04be356a137b40817e7b0ce435a4c9fc5a
Nandaniraj Singh <Nandaniraj.Singh@teknopoint.local>
2026-04-17 11:01:25 +0530

    accordion content model updated

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,25 +1,23 @@
+/*
+ * Accordion Block
+ * Recreate an accordion
+ * https://www.hlx.live/developer/block-collection/accordion
+ */
+
 export default function decorate(block) {
-  const items = block.querySelectorAll(".item");
-
-  items.forEach((item) => {
-    const label = item.querySelector(".title");
-    const body = item.querySelector(".description");
-
-    if (!label || !body) return;
-
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
     const summary = document.createElement("summary");
     summary.className = "accordion-item-label";
     summary.append(...label.childNodes);
-
-    const content = document.createElement("div");
-    content.className = "accordion-item-body";
-    content.append(...body.childNodes);
-
+    // decorate accordion item body
+    const body = row.children[1];
+    body.className = "accordion-item-body";
+    // decorate accordion item
     const details = document.createElement("details");
     details.className = "accordion-item";
-
-    details.append(summary, content);
-
-    item.replaceWith(details);
+    details.append(summary, body);
+    row.replaceWith(details);
   });
 }
