e3e821226d5bd932e42b8d75d7da73690ba1ee1b
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-20 17:31:12 +0530

    updated contact hero js to render image wrapper class

--- blocks/contact-hero/contact-hero.js
+++ blocks/contact-hero/contact-hero.js
@@ -4,6 +4,21 @@ export default function decorate(block) {
     if (rows.length < 2) return;
     let textwrapper = rows[0];
     textwrapper.classList.add('contact-hero-text');
+    if (textWrapper) {
+    const paragraphs = textWrapper.querySelectorAll('p');
+
+    if (paragraphs[0]) {
+      const heading = document.createElement('h1');
+      heading.textContent = paragraphs[0].textContent;
+      textwrapper.appendChild(heading);
+    }
+
+    if (paragraphs[1]) {
+      const desc = document.createElement('p');
+      desc.textContent = paragraphs[1].textContent;
+      textwrapper.appendChild(desc);
+    }
+  }
     let imageWrapper = rows[1];
     imageWrapper.classList.add('contact-hero-image');
    
