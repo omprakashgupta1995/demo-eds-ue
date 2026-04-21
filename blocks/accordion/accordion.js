9f03d6633f75d5f178fc4c5d0b9ee359ea2496c1
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-21 23:10:44 +0530

    ES Lint Fix : updated eslintIgnore and accordion js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,25 +1,32 @@
 /*
  * Accordion Block
  * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
  */
-console.log('accordion block loaded', block);
 
 export default function decorate(block) {
-  if (window.location.href.includes('/author')) return true;
+  if (!block) return;
+
+  // Skip in author mode
+  if (window.location.href.includes('/author')) return;
+
   [...block.children].forEach((row) => {
-    // decorate accordion item label
+    if (row.children.length < 2) return;
+
+    // label
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
-    // decorate accordion item body
+
+    // body
     const body = row.children[1];
     body.className = 'accordion-item-body';
-    // decorate accordion item
+
+    // wrapper
     const details = document.createElement('details');
     details.className = 'accordion-item';
     details.append(summary, body);
+
     row.replaceWith(details);
   });
 }
