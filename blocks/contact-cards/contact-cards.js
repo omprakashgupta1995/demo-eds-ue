d5f557b660910025fa92aef4bd07feb144ec5cc3
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-21 16:20:15 +0530

    added js and css for contact cards

---
+++ blocks/contact-cards/contact-cards.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+    console.log(block);
+    let rows =[...block.children];
+    rows.forEach((row)=>{
+        row.classList.add('contact-card');
+        row.children[0].classList.add('contact-card-imagewrapper');
+        row.children[1].classList.add('contact-card-textwrapper');
+    })
+}
--- blocks/contact-hero/contact-hero.js
+++ blocks/contact-hero/contact-hero.js
@@ -1,17 +1,12 @@
 export default function decorate(block) {
-  
     let rows =[...block.children];
     if (rows.length < 2) return;
-   
     let textwrapper = rows[0];
     textwrapper.classList.add('contact-hero-text');
+    textwrapper.children[0].classList.add('contact-hero-textwrapper');
     let imageWrapper = rows[1];
     let paragraph = textwrapper.querySelectorAll('p');
     paragraph[0].classList.add('contact-hero-title');
     paragraph[1].classList.add('contact-hero-description');
     imageWrapper.classList.add('contact-hero-imagewrapper');
-    
-    console.log(textwrapper);
-    console.log(imageWrapper);
-    
 }
