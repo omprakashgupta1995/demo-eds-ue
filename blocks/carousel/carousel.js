19c7ada28975787a6c736e1191ed1fc6626219e0
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-21 22:58:08 +0530

    ES Lint Build failed fix

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,18 +1,19 @@
-import swiper from "./swiper.min.js";
+import swiper from './swiper.min.js';
+
 export default function decorate(block) {
-    block.parentElement.classList.add('swiper');
-    let blockParent = block.parentElement;
-    block.classList.add('swiper-wrapper');
-    Array.from([...block.children]).forEach((item)=>{
-        item.classList.add('swiper-slide');
-    })
-    let swiperPagination = document.createElement('div');
-    swiperPagination.classList.add('swiper-pagination');
-    blockParent.appendChild(swiperPagination);
-    swiper(blockParent,{
-        pagination: {
-            el: '.swiper-pagination'
-            
-        }
-    })
+  block.parentElement.classList.add('swiper');
+  const blockParent = block.parentElement;
+  block.classList.add('swiper-wrapper');
+  Array.from([...block.children]).forEach((item) => {
+    item.classList.add('swiper-slide');
+  });
+  const swiperPagination = document.createElement('div');
+  swiperPagination.classList.add('swiper-pagination');
+  blockParent.appendChild(swiperPagination);
+  swiper(blockParent, {
+    pagination: {
+      el: '.swiper-pagination',
+
+    },
+  });
 }
--- blocks/contact-cards/contact-cards.js
+++ blocks/contact-cards/contact-cards.js
@@ -1,9 +1,8 @@
 export default function decorate(block) {
-    
-    let rows =[...block.children];
-    rows.forEach((row)=>{
-        row.classList.add('contact-card');
-        row.children[0].classList.add('contact-card-imagewrapper');
-        row.children[1].classList.add('contact-card-textwrapper');
-    })
+  const rows = [...block.children];
+  rows.forEach((row) => {
+    row.classList.add('contact-card');
+    row.children[0].classList.add('contact-card-imagewrapper');
+    row.children[1].classList.add('contact-card-textwrapper');
+  });
 }
