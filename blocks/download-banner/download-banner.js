03075bd6dafb73265f2f442bd9e18f70b0d43c60
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-22 18:44:09 +0530

    added js and css for qr banner

--- blocks/download-banner/download-banner.js
+++ blocks/download-banner/download-banner.js
@@ -1,5 +1,13 @@
 export default function decorate(block) {
     let rows = [...block.children];
-    rows[0].classList.add('banner-content');
-    console.log(rows[0]);
+    let bannerContentWrapper = rows[0];
+    bannerContentWrapper.classList.add('banner-content-wrapper');
+    let bannerContent = bannerContentWrapper.children[0];
+    bannerContent.classList.add('banner-content');
+    let bannerImageWrapper = bannerContent.children[0];
+    bannerImageWrapper.classList.add('banner-image-wrapper');
+    bannerContent.children[1].classList.add('banner-heading');
+    bannerContent.children[2].classList.add('banner-description');
+    rows[1].classList.add('banner-qr-wrapper');
+   
 }
