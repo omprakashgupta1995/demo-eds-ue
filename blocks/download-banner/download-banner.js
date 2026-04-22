947e0aceee3437f95f896777ca879029e1483705
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-22 18:52:57 +0530

    download banner js optimized

--- blocks/download-banner/download-banner.js
+++ blocks/download-banner/download-banner.js
@@ -1,5 +1,6 @@
 export default function decorate(block) {
-    let rows = [...block.children];
+    if(window.location.pathname.includes('/mahesh/contact-us')){
+            let rows = [...block.children];
     let bannerContentWrapper = rows[0];
     bannerContentWrapper.classList.add('banner-content-wrapper');
     let bannerContent = bannerContentWrapper.children[0];
@@ -9,5 +10,16 @@ export default function decorate(block) {
     bannerContent.children[1].classList.add('banner-heading');
     bannerContent.children[2].classList.add('banner-description');
     rows[1].classList.add('banner-qr-wrapper');
+    }
+    // let rows = [...block.children];
+    // let bannerContentWrapper = rows[0];
+    // bannerContentWrapper.classList.add('banner-content-wrapper');
+    // let bannerContent = bannerContentWrapper.children[0];
+    // bannerContent.classList.add('banner-content');
+    // let bannerImageWrapper = bannerContent.children[0];
+    // bannerImageWrapper.classList.add('banner-image-wrapper');
+    // bannerContent.children[1].classList.add('banner-heading');
+    // bannerContent.children[2].classList.add('banner-description');
+    // rows[1].classList.add('banner-qr-wrapper');
    
 }
