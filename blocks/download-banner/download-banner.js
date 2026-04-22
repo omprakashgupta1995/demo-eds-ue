716ea2abc5dca4db14c844c00c32a36ba5287f99
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-22 19:16:56 +0530

    added css and js for download banner

--- blocks/download-banner/download-banner.js
+++ blocks/download-banner/download-banner.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
-    
-    
+    if(window.location.pathname.includes('/mahesh/contact-us')) {
     let rows = [...block.children];
     let bannerContentWrapper = rows[0];
     bannerContentWrapper.classList.add('banner-content-wrapper');
@@ -11,5 +10,10 @@ export default function decorate(block) {
     bannerContent.children[1].classList.add('banner-heading');
     bannerContent.children[2].classList.add('banner-description');
     rows[1].classList.add('banner-qr-wrapper');
+    let bannerQRContent = rows[1].children[0];
+    bannerQRContent.classList.add('banner-qr-content');
+    bannerQRContent.children[0].classList.add('banner-qr-image-wrapper');
+    bannerQRContent.children[1].classList.add('banner-qr-description');
+}
    
 }
