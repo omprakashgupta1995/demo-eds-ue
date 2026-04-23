fcd27badcc15281a0a05f665c883a6034b4a8dc8
Tushar Chaube <Tushar.Chaube@teknopoint.local>
2026-04-23 14:57:56 +0530

    added menu-list block

---
+++ blocks/menu-list/menu-list.js
@@ -0,0 +1,10 @@
+export default function decorate(block) {
+    const rows = [...block.children];
+    if (!rows.length) return;
+    rows.forEach((row) => {
+        row.classList.add('menu-list-item');
+        row?.children[0]?.classList.add('menu-image-wrapper');
+        row?.children[1]?.classList.add('menu-content-wrapper');
+    })
+
+}
