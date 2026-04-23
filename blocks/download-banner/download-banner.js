56a0c272429584dda9c60fa532084d2ff1870d39
Dhruv Parmar <dhruvparmardp29@gmail.com>
2026-04-23 14:46:48 +0530

    created Download Banner

---
+++ blocks/download-banner/download-banner.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+    // second column inner wrapper
+    const contentInner = block.querySelector(":scope > div:nth-child(2) > div");
+
+    if (!contentInner) return;
+
+    const paragraphs = contentInner.querySelectorAll("p");
+
+    if (paragraphs.length < 3) return;
+
+    const qrImagePara = paragraphs[1];
+    const qrTextPara = paragraphs[2];
+
+    // Create wrapper
+    const qrRow = document.createElement("div");
+    qrRow.classList.add("qr-row");
+
+    qrRow.append(qrImagePara, qrTextPara);
+
+    contentInner.append(qrRow);
+}
