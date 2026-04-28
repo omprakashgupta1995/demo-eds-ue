a83a62559f1e4489830867c020eb196fcc77d0ad
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-04-28 12:42:15 +0530

    added styles in section for cards

--- blocks/hero-carousel/hero-carousel.js
+++ blocks/hero-carousel/hero-carousel.js
@@ -2,12 +2,8 @@ import swiper from "./swiper.min.js";
 function resolveDamUrl(path) {
   try {
     const url = new URL(path, window.location.origin);
-
-    // Only keep the DAM path
     if (url.pathname.startsWith("/content/dam")) {
-      
       return `https://publish-p48457-e1275402.adobeaemcloud.com${url.pathname}`;
-     
     }
     return path;
   } catch (e) {
@@ -26,7 +22,7 @@ async function getMediaTypeFromUrl(url) {
   //   // silent fail → fallback below
   // }
 
-  // 🔥 fallback to extension check
+  // fallback to extension check
   const ext = url.split(".").pop().toLowerCase().split("?")[0];
   if (["jpg", "jpeg", "png", "gif", "webp", "svg"].includes(ext))
     return "image";
@@ -38,26 +34,21 @@ async function getMediaTypeFromUrl(url) {
 export default async function decorate(block) {
   const rows = [...block.children];
   if (!rows.length) return;
-
   block.parentElement.classList.add("swiper");
   const blockParent = block.parentElement;
   // create thumbs container
   const thumbsContainer = document.createElement("div");
   thumbsContainer.classList.add("hero-thumbs-swiper", "swiper");
-
   const thumbsWrapper = document.createElement("div");
   thumbsWrapper.classList.add("swiper-wrapper", "thumbs-swiper-wrapper");
-
   thumbsContainer.appendChild(thumbsWrapper);
-
   blockParent.appendChild(thumbsContainer);
   block.classList.add("swiper-wrapper");
 
-  // 🔥 process all rows in parallel
+  //  process all rows in parallel
   await Promise.all(
     rows.map(async (row) => {
       row.classList.add("hero-carousel-slide", "swiper-slide");
-
       const wrapper = row.children?.[0];
       const ctaWrapper = row.children?.[1];
       ctaWrapper.classList.add('ctaWrapper');
@@ -65,26 +56,20 @@ export default async function decorate(block) {
           let ctaButton =ctaWrapper?.children?.[0];
           ctaButton.classList.add('cta-button');
       }
-      
       const thumbnailNode = row.children?.[2]; // your 3rd column
       const thumbNail_img = thumbnailNode?.querySelector("img");
       if (thumbNail_img) {
         const thumbSlide = document.createElement("div");
         thumbSlide.classList.add("hero-thumb-swiper-slide", "swiper-slide");
-
         const thumbImg = document.createElement("img");
         thumbImg.src = thumbNail_img.src;
         thumbImg.classList.add("hero-thumb-image");
-
         thumbSlide.appendChild(thumbImg);
         thumbsWrapper.appendChild(thumbSlide);
       }
-      // remove from main slide (important)
       thumbnailNode?.remove();
       if (!wrapper) return;
-
       wrapper.classList.add("slide-image-wrapper");
-
       const anchor = wrapper.querySelector("a");
       if (anchor) {
         const url = resolveDamUrl(anchor.href);
@@ -92,7 +77,6 @@ export default async function decorate(block) {
         const type = await getMediaTypeFromUrl(url);
         console.log(type);
         let mediaElement;
-
         if (type === "image") {
           mediaElement = document.createElement("img");
           mediaElement.src = url;
@@ -113,7 +97,6 @@ export default async function decorate(block) {
         // anchor.replaceWith(mediaElement);
       } else {
         const heroCarouselImage = wrapper.querySelector("img");
-
         if (heroCarouselImage) {
           console.log("IMAGE FOUND");
           heroCarouselImage.classList.add("hero-carousel-image");
@@ -121,14 +104,12 @@ export default async function decorate(block) {
       }
     }),
   );
-
   const thumbsSwiper = new swiper(thumbsContainer, {
     slidesPerView: 'auto',
     spaceBetween: 12,
     watchSlidesProgress: true,
     slideToClickedSlide: true,
   });
-  // 🔥 init swiper after DOM is ready
   const mainSwiper = new swiper(blockParent, {
   loop: true,
   loopedSlides: rows.length,
