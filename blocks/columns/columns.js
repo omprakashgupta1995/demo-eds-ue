67eb67ae2ec22aab0bc751c2f10a8fa31b79a5f4
Mahesh Kamble <Mahesh.Kamble@teknopoint.local>
2026-05-05 16:17:03 +0530

    added arrow svg in icon folder

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -21,18 +21,48 @@ export default function decorate(block) {
 
   if(block && block.classList.contains('our-range-h')){
     const homePage_ourRange = block?.children?.[0];
+    if(!homePage_ourRange) return;
     homePage_ourRange.classList.add('homepage-our-range');
     const ourRangeTextWrapper = homePage_ourRange?.children?.[0];
     const ourRangeButtonWrpper = homePage_ourRange?.children?.[1];
-    ourRangeTextWrapper.classList.add('ourRange-text-wrapper');
-    ourRangeTextWrapper?.children?.[0].classList.add('our-range-title');
-    ourRangeTextWrapper?.children?.[1].classList.add('our-range-heading');
-    if(ourRangeButtonWrpper && ourRangeButtonWrpper.children.length > 0){
-      ourRangeButtonWrpper.classList.add('ourRange-button-wrapper');
-    }else{
+
+    if(ourRangeTextWrapper){
+       ourRangeTextWrapper.classList.add('ourRange-text-wrapper');
+       ourRangeTextWrapper?.children?.[0].classList.add('our-range-title');
+       ourRangeTextWrapper?.children?.[1].classList.add('our-range-heading');
+    }
+   
+    if(ourRangeButtonWrpper){
+      const hasLink = ourRangeButtonWrpper.querySelector('a');
+      if(hasLink){
+        ourRangeButtonWrpper.classList.add('ourRange-button-wrapper');
+      }
+     else{
       ourRangeButtonWrpper.remove();
+    } 
     }
     
+  }
+
+  if(block && block.classList.contains('our-range-cards')){
+    const rangeCardsWrapper = block?.children?.[0];
+    if(rangeCardsWrapper){
+      rangeCardsWrapper.classList.add('range-cards-wrapper');
+      [...rangeCardsWrapper.children].forEach((row)=>{
+        row.classList.add('range-card');
+
+        const cardPicture = row.querySelector('picture');
+        const cardImage = cardPicture.querySelector('img');
+        cardImage.classList.add('range-card-image');
+        const imageWrapper = cardPicture.parentElement;
+        imageWrapper.classList.add('card-image-wrapper');
+        const link = row.querySelector('a');
+        const buttonContainer = link.parentElement;
+        buttonContainer.classList.add('card-button');
+        const ImageText = row.children?.[2];
+        ImageText.classList.add('image-text');
+      })
+    }
   }
 
 }
